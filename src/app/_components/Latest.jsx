9a47f0d84d3bc33d8c552167de1aa834b70edf58
hambali-020608 <subastianhambali@gmail.com>
2026-01-13 09:56:12 +0700

    fix card

--- src/app/_components/Latest.jsx
+++ src/app/_components/Latest.jsx
@@ -29,18 +29,19 @@ export default function Latest(){
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
                     {books.books.map((book) => (
-                      <div className="card bg-base-100 w-[200px] h-[600px] shadow-sm">
+                      <div className="card bg-base-100 w-[200px] h-auto card-xs  shadow-xl">
   <figure className="px-10 pt-10">
     <img
       src={book.image}
-      alt="Shoes"
-      className="rounded-xl" />
+      alt={book.title}
+      className="rounded-xl h-56" />
   </figure>
   <div className="card-body items-center text-center">
     <h2 className="card-title">{book.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <p>{book.subtitle}</p>
+    <p className="text-xs italic">By {book.authors}</p>
     <div className="card-actions">
-      <button className="btn btn-primary">Read Books</button>
+      <button className="btn btn-warning">Read Books</button>
     </div>
   </div>
 </div>
